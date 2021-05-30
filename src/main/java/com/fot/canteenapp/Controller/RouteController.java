cebdc8e0d07f93bb49e81eef5618f248fffe0818
Ruvindu Madhushanka <53707613+Ruvindu@users.noreply.github.com>
2021-05-30 14:14:36 +0530

    Initial commit

---
+++ src/main/java/com/fot/canteenapp/Controller/RouteController.java
@@ -0,0 +1,26 @@
+package com.fot.canteenapp.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class RouteController {
+
+    @GetMapping(path = "/")
+    public String navigatetohome() {
+        return "home";
+    }
+
+    @GetMapping(path = "/signin")
+    public String navigatetosignin() {
+        return "signin";
+    }
+
+    @GetMapping(path = "/signup")
+    public String navigatetosignout() {
+        return "signup";
+    }
+}
