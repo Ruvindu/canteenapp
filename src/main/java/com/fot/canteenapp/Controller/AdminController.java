269811a9a2c3d9e66cd169ec106533919bf86e36
Hameesha Rantharu <45784711+hameesha@users.noreply.github.com>
2021-07-02 02:05:12 +0530

    updates

--- src/main/java/com/fot/canteenapp/Controller/AdminController.java
+++ src/main/java/com/fot/canteenapp/Controller/AdminController.java
@@ -19,15 +19,24 @@ public class AdminController {
 
     @RequestMapping("/dashboard")
     public String viewHomePage(Model model, HttpSession session){
-
+        
 //        user authenticate
         if (!auth.getAuth().is_admin(session))
             return "redirect:/signin";
+
 
         List<Inventory> listProducts = invser.getAllItems();
         Inventory item = new Inventory();
         model.addAttribute("item", item);
         model.addAttribute("listProducts",listProducts);
+        List<String> user_s = (List<String>) session.getAttribute("USER_SESSION");
+
+        if (user_s == null) {
+//            user_s = new ArrayList<>();
+            model.addAttribute("sess", null);
+        }else{
+            model.addAttribute("sess", user_s);
+        }
         return "admin_template/admin_dashboard";
     }
 
