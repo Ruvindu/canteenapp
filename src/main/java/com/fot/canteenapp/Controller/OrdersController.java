61de41aa59633a129b0709daf9613e5f7125fdcf
Ruvindu Madhushanka <53707613+Ruvindu@users.noreply.github.com>
2021-07-06 12:56:40 +0530

    order update

--- src/main/java/com/fot/canteenapp/Controller/OrdersController.java
+++ src/main/java/com/fot/canteenapp/Controller/OrdersController.java
@@ -34,7 +34,7 @@ public class OrdersController {
         List<String> user_s = (List<String>) session.getAttribute("USER_SESSION");
         String to = user_s.get(2);
         String body = "Order id - "+order.getOrderId()+"\nQuantitiy - "+order.getItemQty()+"\nAmount - "+ order.getTotalPrice();
-        
+//        System.out.println(body);
         mailser.sendSimpleEmail(to,body);
         return "redirect:/?success";
     }
