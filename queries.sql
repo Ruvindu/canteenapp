ad86323be23e11ec1069fc60e94292cc18952b98
dishan97 <79416597+dishan97@users.noreply.github.com>
2021-07-06 12:00:42 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -23,8 +23,10 @@ AFTER INSERT ON `orders` FOR EACH ROW
 	UPDATE `inventory` SET `item_qty`= `item_qty`- NEW.order_qty WHERE `item_id`= NEW.item_id;
  END
 
-
-CREATE PROCEDURE `getDailySales`() BEGIN DECLARE sum_total INT DEFAULT 0; SELECT SUM(orders.total_price) into sum_total FROM `inventory`,`orders` WHERE `inventory`.`item_id` = `orders`.`item_id` AND DATE(`orders`.`order_date`) = CURRENT_DATE; INSERT INTO `sales` (`date`, `total_sales`) VALUES (CURRENT_DATE, sum_total); END
+DELIMITER //  
+CREATE PROCEDURE `getDailySales`()
+	BEGIN DECLARE sum_total INT DEFAULT 0; SELECT SUM(orders.total_price) into sum_total FROM `inventory`,`orders` WHERE `inventory`.`item_id` = `orders`.`item_id` AND DATE(`orders`.`order_date`) = CURRENT_DATE; INSERT INTO `sales` (`date`, `total_sales`) VALUES (CURRENT_DATE, sum_total); END
+END //
 
 CREATE EVENT `sales_event` ON SCHEDULE EVERY 1 DAY STARTS '2021-07-03 20:00:00' ON COMPLETION PRESERVE ENABLE DO CALL getDailySales() 
 
