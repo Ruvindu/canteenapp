628b387e083185937f4eec7ade488807fcfe208f
Ruvindu Madhushanka <53707613+Ruvindu@users.noreply.github.com>
2021-07-20 14:07:13 +0530

    query updated

--- queries.sql
+++ queries.sql
@@ -28,7 +28,7 @@ CREATE PROCEDURE `getDailySales`()
 	BEGIN DECLARE sum_total INT DEFAULT 0; SELECT SUM(orders.total_price) into sum_total FROM `inventory`,`orders` WHERE `inventory`.`item_id` = `orders`.`item_id` AND DATE(`orders`.`order_date`) = CURRENT_DATE; INSERT INTO `sales` (`date`, `total_sales`) VALUES (CURRENT_DATE, sum_total); END
 END //
 
-CREATE EVENT `sales_event` ON SCHEDULE EVERY 1 DAY STARTS '2021-07-03 20:00:00' ON COMPLETION PRESERVE ENABLE DO CALL getDailySales() 
+CREATE EVENT `sales_event` ON SCHEDULE EVERY 1 DAY STARTS '2021-07-03 20:00:00' ON COMPLETION PRESERVE ENABLE DO CALL getDailySales()
 
 DROP EVENT `sales_event`; CREATE DEFINER=`root`@`localhost` EVENT `sales_event` ON SCHEDULE EVERY 1 DAY STARTS '2021-07-06 11:15:11' ON COMPLETION PRESERVE ENABLE DO CALL getDailySales()
 
@@ -36,3 +36,31 @@ DELIMITER //
 CREATE PROCEDURE sales_event()
 BEGIN DECLARE sum_total INT DEFAULT 0; SELECT SUM(orders.total_price) into sum_total FROM `inventory`,`orders` WHERE `inventory`.`item_id` = `orders`.`item_id` AND DATE(`orders`.`order_date`) = CURRENT_DATE AND orders.status = 3; INSERT INTO `sales` (`date`, `total_sales`) VALUES (CURRENT_DATE, sum_total); 
 END //
+
+
+
+DELIMITER //
+CREATE PROCEDURE get_orders_by_date(IN d Date)
+BEGIN
+SELECT * FROM `orders` WHERE DATE(`order_date`)=d;
+END //
+
+
+DELIMITER $$
+CREATE FUNCTION totalSale(
+    d date
+)
+    RETURNS int
+    DETERMINISTIC
+BEGIN
+    DECLARE sale int;
+
+SELECT SUM(total_price) INTO sale FROM `orders` WHERE DATE(`order_date`)=d;
+
+RETURN (sale);
+END$$
+DELIMITER ;
+
+
+
+CREATE VIEW `view_cart` AS  select `c`.`cart_id` AS `cart_id`,`c`.`item_id` AS `item_id`,`c`.`user_id` AS `user_id`,`i`.`item_name` AS `item_name`,`i`.`item_price` AS `item_price`,`i`.`item_qty` AS `item_qty` from (`inventory` `i` join `cart` `c`) where (`c`.`item_id` = `i`.`item_id`) ;
